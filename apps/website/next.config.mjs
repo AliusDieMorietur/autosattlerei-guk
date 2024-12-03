7bd2c2fa470109fcbe72ba1899cdf22150edbbc7
AliusDieMorietur <iakaminskij@gmail.com>
2024-12-04 00:45:28 +0200

    Fix next

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -8,7 +8,8 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/:path*`,
+        // destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/:path*`,
+        destination: `http://0.0.0.0:8080/:path*`,
       },
     ];
   },
