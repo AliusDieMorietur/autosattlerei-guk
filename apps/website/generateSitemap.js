a09ee5c81b3afc34cd81693f66e6bd899d6e9395
AliusDieMorietur <iakaminskij@gmail.com>
2024-12-16 23:43:22 +0200

    Add sitemap

---
+++ apps/website/generateSitemap.js
@@ -0,0 +1,40 @@
+const { writeFileSync } = require("fs");
+
+{
+  /* <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemap>
+<loc>https://sitemap.subjektiv.art/sitemap.xml</loc>
+<lastmod>2024-10-02</lastmod>
+</sitemap>
+</sitemapindex> */
+}
+
+const URLS = [
+  "/",
+  "/gallery",
+  "/notfound",
+  "/impressum",
+  "/wheel",
+  "/door-panel",
+  "/salon",
+];
+
+const HOST = "https://autosattlerei-guk.de";
+
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth() + 1;
+const day = now.getDate();
+const date = `${year}-${month}-${day}`;
+
+let sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+for (const url of URLS) {
+  sitemap += "<url>";
+  sitemap += `<loc>${HOST}${url}</loc>`;
+  sitemap += `<lastmod>${date}</lastmod>`;
+  sitemap += "</url>";
+}
+sitemap += `</urlset>`;
+
+writeFileSync("./public/sitemap.xml", sitemap);
