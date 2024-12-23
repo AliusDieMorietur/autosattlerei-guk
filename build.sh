e280ba24f4149565b0ab205cb7c4cd87632e5f0e
AliusDieMorietur <iakaminskij@gmail.com>
2024-12-23 21:14:13 +0200

    Update build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 export POSTGRES_URL=postgresql://main:example@0.0.0.0:1234/main
 export SECRET_TOKEN=42
 export NEXT_PUBLIC_SERVER_URL=http://0.0.0.0:6732
+export NEXT_PUBLIC_HOST_URL=https://autosattlerei-guk.de
 export POSTGRES_USER=main
 export POSTGRES_PASSWORD=example
 export POSTGRES_DB=main
