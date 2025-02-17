1680bcdde65e0672a04af99f6738aa7c30884362
AliusDieMorietur <iakaminskij@gmail.com>
2025-02-17 21:18:30 +0200

    Fix cannonical

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,101 +1,99 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "../globals.css";
+import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import { routing } from "@/i18n/routing";
-import { notFound, redirect } from "next/navigation";
-import { Header } from "./Header";
+import { Header } from "../components/Header";
 
 const Poppins = localFont({
   src: [
     {
-      path: "../fonts/Poppins/Poppins-Thin.ttf",
+      path: "./fonts/Poppins/Poppins-Thin.ttf",
       weight: "100",
       style: "normal",
     },
     {
-      path: "../fonts/Poppins/Poppins-ThinItalic.ttf",
+      path: "./fonts/Poppins/Poppins-ThinItalic.ttf",
       weight: "100",
       style: "italic",
     },
     {
-      path: "../fonts/Poppins/Poppins-ExtraLight.ttf",
+      path: "./fonts/Poppins/Poppins-ExtraLight.ttf",
       weight: "200",
       style: "normal",
     },
     {
-      path: "../fonts/Poppins/Poppins-ExtraLightItalic.ttf",
+      path: "./fonts/Poppins/Poppins-ExtraLightItalic.ttf",
       weight: "200",
       style: "italic",
     },
     {
-      path: "../fonts/Poppins/Poppins-Light.ttf",
+      path: "./fonts/Poppins/Poppins-Light.ttf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../fonts/Poppins/Poppins-LightItalic.ttf",
+      path: "./fonts/Poppins/Poppins-LightItalic.ttf",
       weight: "300",
       style: "italic",
     },
     {
-      path: "../fonts/Poppins/Poppins-Regular.ttf",
+      path: "./fonts/Poppins/Poppins-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../fonts/Poppins/Poppins-Italic.ttf",
+      path: "./fonts/Poppins/Poppins-Italic.ttf",
       weight: "400",
       style: "italic",
     },
     {
-      path: "../fonts/Poppins/Poppins-Medium.ttf",
+      path: "./fonts/Poppins/Poppins-Medium.ttf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../fonts/Poppins/Poppins-MediumItalic.ttf",
+      path: "./fonts/Poppins/Poppins-MediumItalic.ttf",
       weight: "500",
       style: "italic",
     },
     {
-      path: "../fonts/Poppins/Poppins-SemiBold.ttf",
+      path: "./fonts/Poppins/Poppins-SemiBold.ttf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../fonts/Poppins/Poppins-SemiBoldItalic.ttf",
+      path: "./fonts/Poppins/Poppins-SemiBoldItalic.ttf",
       weight: "600",
       style: "italic",
     },
     {
-      path: "../fonts/Poppins/Poppins-Bold.ttf",
+      path: "./fonts/Poppins/Poppins-Bold.ttf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../fonts/Poppins/Poppins-BoldItalic.ttf",
+      path: "./fonts/Poppins/Poppins-BoldItalic.ttf",
       weight: "700",
       style: "italic",
     },
     {
-      path: "../fonts/Poppins/Poppins-ExtraBold.ttf",
+      path: "./fonts/Poppins/Poppins-ExtraBold.ttf",
       weight: "800",
       style: "normal",
     },
     {
-      path: "../fonts/Poppins/Poppins-ExtraBoldItalic.ttf",
+      path: "./fonts/Poppins/Poppins-ExtraBoldItalic.ttf",
       weight: "800",
       style: "italic",
     },
     {
-      path: "../fonts/Poppins/Poppins-Black.ttf",
+      path: "./fonts/Poppins/Poppins-Black.ttf",
       weight: "900",
       style: "normal",
     },
     {
-      path: "../fonts/Poppins/Poppins-BlackItalic.ttf",
+      path: "./fonts/Poppins/Poppins-BlackItalic.ttf",
       weight: "900",
       style: "italic",
     },
@@ -108,52 +106,52 @@ const Poppins = localFont({
 const OpenSans = localFont({
   src: [
     {
-      path: "../fonts/OpenSans/static/OpenSans-Light.ttf",
+      path: "./fonts/OpenSans/static/OpenSans-Light.ttf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../fonts/OpenSans/static/OpenSans-LightItalic.ttf",
+      path: "./fonts/OpenSans/static/OpenSans-LightItalic.ttf",
       weight: "300",
       style: "italic",
     },
     {
-      path: "../fonts/OpenSans/static/OpenSans-Regular.ttf",
+      path: "./fonts/OpenSans/static/OpenSans-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../fonts/OpenSans/static/OpenSans-Italic.ttf",
+      path: "./fonts/OpenSans/static/OpenSans-Italic.ttf",
       weight: "400",
       style: "italic",
     },
     {
-      path: "../fonts/OpenSans/static/OpenSans-SemiBold.ttf",
+      path: "./fonts/OpenSans/static/OpenSans-SemiBold.ttf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../fonts/OpenSans/static/OpenSans-SemiBoldItalic.ttf",
+      path: "./fonts/OpenSans/static/OpenSans-SemiBoldItalic.ttf",
       weight: "600",
       style: "italic",
     },
     {
-      path: "../fonts/OpenSans/static/OpenSans-Bold.ttf",
+      path: "./fonts/OpenSans/static/OpenSans-Bold.ttf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../fonts/OpenSans/static/OpenSans-BoldItalic.ttf",
+      path: "./fonts/OpenSans/static/OpenSans-BoldItalic.ttf",
       weight: "700",
       style: "italic",
     },
     {
-      path: "../fonts/OpenSans/static/OpenSans-ExtraBold.ttf",
+      path: "./fonts/OpenSans/static/OpenSans-ExtraBold.ttf",
       weight: "800",
       style: "normal",
     },
     {
-      path: "../fonts/OpenSans/static/OpenSans-ExtraBoldItalic.ttf",
+      path: "./fonts/OpenSans/static/OpenSans-ExtraBoldItalic.ttf",
       weight: "800",
       style: "italic",
     },
@@ -167,13 +165,16 @@ export const metadata: Metadata = {
   title: "Autosattlerei Guk",
   description:
     "Autosattlerei Guk in Berlin - Eine Werkstatt für das Neubeziehen von Fahrzeuginnenräumen. Arbeiten in allen Schwierigkeitsgraden. Vielfältige Arbeitsoptionen: Neubezug von Türverkleidungen, Sitzen, Lenkrädern, Armaturenbrettern, Dachhimmeln, Griffen, Schalthebeln und kompletten Innenräumen. Von Oldtimern bis hin zu Supersportwagen. Handwerkskunst auf höchstem Niveau.",
+  alternates: {
+    canonical: "./",
+  },
 };
 
 export default async function RootLayout(
   props: Readonly<{
     children: React.ReactNode;
     params: Promise<{ locale: string }>;
-  }>,
+  }>
 ) {
   const params = await props.params;
 
