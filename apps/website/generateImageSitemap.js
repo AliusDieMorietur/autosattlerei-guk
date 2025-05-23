52d74ca381b3fcfec222a524106df9a37001971e
AliusDieMorietur <iakaminskij@gmail.com>
2025-05-23 16:05:27 +0300

    feat(website): update sitemap

--- apps/website/generateImageSitemap.js
+++ apps/website/generateImageSitemap.js
@@ -35,8 +35,8 @@ const urls = findImageFiles(IMAGE_DIR);
 
 const now = new Date();
 const year = now.getFullYear();
-const month = now.getMonth() + 1;
-const day = now.getDate();
+const month = String(now.getMonth() + 1).padStart(2, "0");
+const day = String(now.getDate()).padStart(2, "0");
 const date = `${year}-${month}-${day}`;
 
 let sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">`;
--- apps/website/generateSitemap.js
+++ apps/website/generateSitemap.js
@@ -17,8 +17,8 @@ const urls = LOCALES.flatMap((locale) => URLS.map((url) => `/${locale}${url}`));
 
 const now = new Date();
 const year = now.getFullYear();
-const month = now.getMonth() + 1;
-const day = now.getDate();
+const month = String(now.getMonth() + 1).padStart(2, "0");
+const day = String(now.getDate()).padStart(2, "0");
 const date = `${year}-${month}-${day}`;
 
 let sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
